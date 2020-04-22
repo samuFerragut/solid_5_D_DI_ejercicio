a53d0e94c603e5ba58d9842d34a1e6c88e85a84a
samuFerragut <sferragut@cifpfbmoll.eu>
2020-04-22 12:34:07 +0200

    feat(Inventario, InventaarioBBDD): Utilizar interfaz inventario

--- src/org/formacion/di/bbdd/Inventario.java
+++ src/org/formacion/di/bbdd/Inventario.java
@@ -1,5 +1,6 @@
 package org.formacion.di.bbdd;
 
 public interface Inventario {
+
     public int numeroProductos(String tienda, String producto);
 }
--- src/org/formacion/di/bbdd/InventarioBBDD.java
+++ src/org/formacion/di/bbdd/InventarioBBDD.java
@@ -1,12 +1,12 @@
 package org.formacion.di.bbdd;
 
 
-public class InventarioBBDD {
+public class InventarioBBDD implements Inventario {
 
 
 	// Metodo de consulta a la base de datos
-	
-	public int numeroProductos(String tienda, String producto) {
+	@Override
+	public int numeroProducto(String tienda, String producto) {
 		return BBDD.stocs.get(tienda).get(producto);
 	}
 }
